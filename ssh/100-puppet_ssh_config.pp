de7206b8ea3364973143ae84fbbb8fa86d46103b
sjamillah <j.ssozi@alustudent.com>
2023-11-18 08:54:40 -0800

    puppet using to make changes on the config file

---
+++ ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#Script to use puppet to make change on the config file
+
+file { 'etc/ssh/ssh_config':
+        ensure => present,
+
+content => "
+
+        #SSH client configuration
+        host*
+        IdentityFile ~/.ssh/school
+        PasswordAuthentication no
+
+}
