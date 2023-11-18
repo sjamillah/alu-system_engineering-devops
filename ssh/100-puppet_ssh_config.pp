f9b5cf8fb19cbcaae9e6372738f565b6cab0a482
sjamillah <j.ssozi@alustudent.com>
2023-11-18 09:07:08 -0800

    puppet using to make changes on the config file

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 #Script to use puppet to make change on the config file
 
-file { 'ect/ssh/ssh_cofig':
-        ensure => present,
-
-content =>"
-
-        #SSH client configuration
-        host*
-        IdentityFile ~/.ssh/school
-        PasswordAuthentication no
-	",
-
+include stdlib
+file_line { 'private keyphers':
+ensure => present,
+path   => '/etc/ssh/ssh_config',
+line   => 'IdentityFile ~/.ssh/school',
+}
+file_line { 'refuse authenticate using a password':
+ensure => present,
+path   => '/etc/ssh/ssh_config',
+line   => 'PasswordAuthentication no',
 }
